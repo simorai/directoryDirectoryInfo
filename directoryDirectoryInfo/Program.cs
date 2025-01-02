11e7f44077992a09edfe27f5692c9b520b2847e9
simorai <109289730+simorai@users.noreply.github.com>
2025-01-02 00:09:19 +0000

    Add project files.

---
+++ directoryDirectoryInfo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace directoryDirectoryInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
